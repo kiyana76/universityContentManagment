b0aaaf46b459553f36242779d592ec80e9620d14
kiyana76 <kiyana76@gmail.com>
2021-03-03 13:07:10 +0330

    home page ok

--- app/Http/Controllers/Guest/HomeController.php
+++ app/Http/Controllers/Guest/HomeController.php
@@ -3,13 +3,44 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
-use App\Models\Resource;
+use App\Models\{Book, Exam, Note, Question, Resource};
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index() {
-        $resources = Resource::approve()->get()->take(20);
-        return view('guest.index', compact('resources'));
+        $resources = Resource::approve()
+                            ->orderBy('created_at')
+                            ->get()
+                            ->take(20);
+
+        $books = Book::whereHas('resource', function ($q){
+                    $q->whereStatus('approve');
+                    })
+                    ->orderBy('created_at')
+                    ->get()
+                    ->take(4);
+
+        $notes = Note::whereHas('resource', function ($q){
+                    $q->whereStatus('approve');
+                })
+                    ->orderBy('created_at')
+                    ->get()
+                    ->take(4);
+
+        $questions = Question::whereHas('resource', function ($q){
+                    $q->whereStatus('approve');
+                })
+                    ->orderBy('created_at')
+                    ->get()
+                    ->take(4);
+
+        $exams = Exam::whereHas('resource', function ($q){
+                    $q->whereStatus('approve');
+                })
+                    ->orderBy('created_at')
+                    ->get()
+                    ->take(4);
+        return view('guest.index', compact('resources', 'books', 'notes', 'exams', 'questions'));
     }
 }
--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -16,6 +16,7 @@
 <div class="row row-masnory row-tb-20">
     <div class="page-content">
         <div class="col-xs-12">
+            <h3 class="h-title mb-30">جدیدترین ها</h3>
             <div class="owl-carousel owl-theme">
                 @foreach($resources as $item)
                     <div class="coupon-single panel t-center item">
@@ -35,6 +36,146 @@
                         </div>
                     </div>
                 @endforeach
+            </div>
+        </div>
+
+        <div class="col-xs-12">
+            <div class="panel pt-20 pb-30 prl-20 mtb-20">
+                <h3 class="h-title mb-30">کتاب‌ها</h3>
+                <div class="row">
+                    @foreach($books as $item)
+                        <div class="col-sm-4">
+                            <article class="entry panel">
+                                <figure class="entry-media post-thumbnail embed-responsive embed-responsive-16by9" data-bg-img="{{ url('storage/' . $item->resource->thumbnail_image) }}">
+                                </figure>
+                                <div class="entry-wrapper pt-20 pb-10 prl-20">
+                                    <header class="entry-header">
+                                        <h4 class="entry-title mb-10 mb-md-15 font-xs-16 font-sm-18 font-md-16 font-lg-16">
+                                            <a href="#">{{ $item->full_name }}</a>
+                                        </h4>
+                                        <div class="entry-meta mb-10">
+                                            <ul class="tag-info list-inline">
+                                                <li><i class="icon fa fa-user"></i>از طرف: {{ $item->resource->user->full_name }}</li>
+                                                <li><i class="icon fa fa-clock-o"></i>{{ $item->resource->jalali_created_at }}</li>
+                                            </ul>
+                                        </div>
+                                    </header>
+                                    <div class="entry-content">
+                                        <p class="entry-summary">{{ \Illuminate\Support\Str::words($item->resource->description, 8) }}</p>
+                                    </div>
+                                    <footer class="entry-footer text-left">
+                                        <a href="#" class="more-link btn btn-link">مشاهده<i class="icon fa fa-long-arrow-left"></i></a>
+                                    </footer>
+                                </div>
+                            </article>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12">
+            <div class="panel pt-20 pb-30 prl-20 mtb-20">
+                <h3 class="h-title mb-30">جزوه‌ها</h3>
+                <div class="row">
+                    @foreach($notes as $item)
+                        <div class="col-sm-4">
+                            <article class="entry panel">
+                                <figure class="entry-media post-thumbnail embed-responsive embed-responsive-16by9" data-bg-img="{{ url('storage/' . $item->resource->thumbnail_image) }}">
+                                </figure>
+                                <div class="entry-wrapper pt-20 pb-10 prl-20">
+                                    <header class="entry-header">
+                                        <h4 class="entry-title mb-10 mb-md-15 font-xs-16 font-sm-18 font-md-16 font-lg-16">
+                                            <a href="#">{{ $item->full_name }}</a>
+                                        </h4>
+                                        <div class="entry-meta mb-10">
+                                            <ul class="tag-info list-inline">
+                                                <li><i class="icon fa fa-user"></i>از طرف: {{ $item->resource->user->full_name }}</li>
+                                                <li><i class="icon fa fa-clock-o"></i>{{ $item->resource->jalali_created_at }}</li>
+                                            </ul>
+                                        </div>
+                                    </header>
+                                    <div class="entry-content">
+                                        <p class="entry-summary">{{ \Illuminate\Support\Str::words($item->resource->description, 8) }}</p>
+                                    </div>
+                                    <footer class="entry-footer text-left">
+                                        <a href="#" class="more-link btn btn-link">مشاهده<i class="icon fa fa-long-arrow-left"></i></a>
+                                    </footer>
+                                </div>
+                            </article>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12">
+            <div class="panel pt-20 pb-30 prl-20 mtb-20">
+                <h3 class="h-title mb-30">نمونه سوالات</h3>
+                <div class="row">
+                    @foreach($questions as $item)
+                        <div class="col-sm-4">
+                            <article class="entry panel">
+                                <figure class="entry-media post-thumbnail embed-responsive embed-responsive-16by9" data-bg-img="{{ url('storage/' . $item->resource->thumbnail_image) }}">
+                                </figure>
+                                <div class="entry-wrapper pt-20 pb-10 prl-20">
+                                    <header class="entry-header">
+                                        <h4 class="entry-title mb-10 mb-md-15 font-xs-16 font-sm-18 font-md-16 font-lg-16">
+                                            <a href="#">{{ $item->full_name }}</a>
+                                        </h4>
+                                        <div class="entry-meta mb-10">
+                                            <ul class="tag-info list-inline">
+                                                <li><i class="icon fa fa-user"></i>از طرف: {{ $item->resource->user->full_name }}</li>
+                                                <li><i class="icon fa fa-clock-o"></i>{{ $item->resource->jalali_created_at }}</li>
+                                            </ul>
+                                        </div>
+                                    </header>
+                                    <div class="entry-content">
+                                        <p class="entry-summary">{{ \Illuminate\Support\Str::words($item->resource->description, 8) }}</p>
+                                    </div>
+                                    <footer class="entry-footer text-left">
+                                        <a href="#" class="more-link btn btn-link">مشاهده<i class="icon fa fa-long-arrow-left"></i></a>
+                                    </footer>
+                                </div>
+                            </article>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12">
+            <div class="panel pt-20 pb-30 prl-20 mtb-20">
+                <h3 class="h-title mb-30">سوالات کنکور</h3>
+                <div class="row">
+                    @foreach($exams as $item)
+                        <div class="col-sm-4">
+                            <article class="entry panel">
+                                <figure class="entry-media post-thumbnail embed-responsive embed-responsive-16by9" data-bg-img="{{ url('storage/' . $item->resource->thumbnail_image) }}">
+                                </figure>
+                                <div class="entry-wrapper pt-20 pb-10 prl-20">
+                                    <header class="entry-header">
+                                        <h4 class="entry-title mb-10 mb-md-15 font-xs-16 font-sm-18 font-md-16 font-lg-16">
+                                            <a href="#">{{ $item->full_name }}</a>
+                                        </h4>
+                                        <div class="entry-meta mb-10">
+                                            <ul class="tag-info list-inline">
+                                                <li><i class="icon fa fa-user"></i>از طرف: {{ $item->resource->user->full_name }}</li>
+                                                <li><i class="icon fa fa-clock-o"></i>{{ $item->resource->jalali_created_at }}</li>
+                                            </ul>
+                                        </div>
+                                    </header>
+                                    <div class="entry-content">
+                                        <p class="entry-summary">{{ \Illuminate\Support\Str::words($item->resource->description, 8) }}</p>
+                                    </div>
+                                    <footer class="entry-footer text-left">
+                                        <a href="#" class="more-link btn btn-link">مشاهده<i class="icon fa fa-long-arrow-left"></i></a>
+                                    </footer>
+                                </div>
+                            </article>
+                        </div>
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
